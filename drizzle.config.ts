920fbe5563762a646cc4e529528690ea04f7f9b2
Pranav <38887390+thecmdrunner@users.noreply.github.com>
2025-05-30 14:27:58 +0530

    t3

---
+++ drizzle.config.ts
@@ -0,0 +1,12 @@
+import { type Config } from "drizzle-kit";
+
+import { env } from "@/env";
+
+export default {
+  schema: "./src/server/db/schema.ts",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: env.DATABASE_URL,
+  },
+  tablesFilter: ["100x-elevenlabs-hackathon-2025_*"],
+} satisfies Config;
