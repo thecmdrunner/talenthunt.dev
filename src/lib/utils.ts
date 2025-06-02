c4298c3d2df045a8a50d93c631dd882b58737941
Pranav <38887390+thecmdrunner@users.noreply.github.com>
2025-06-02 09:38:03 +0530

    scripts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,16 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function formatShortNumber(number: number) {
+  if (number >= 1000000) {
+    return `${(number / 1000000).toFixed(1)}M`;
+  } else if (number >= 1000) {
+    return `${(number / 1000).toFixed(1)}k`;
+  } else {
+    return number.toLocaleString();
+  }
 }
